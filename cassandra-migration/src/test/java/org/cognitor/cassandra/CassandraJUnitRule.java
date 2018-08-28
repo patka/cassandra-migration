7a544dda4825497d89ba5f72ba2309dcb4fb3a65
Patrick Kranz <patrick@got-email.de>
2018-08-28 09:24:47 +0200

    Updated CasssandraUnit to the latest version. As this comes with logback classic as a dependency I removed log4j for testing to get rid of the double binding warning of slf4j. Made some changes to the CassandraJUnitRule that CassandraUnit works again.

--- cassandra-migration/src/test/java/org/cognitor/cassandra/CassandraJUnitRule.java
+++ cassandra-migration/src/test/java/org/cognitor/cassandra/CassandraJUnitRule.java
@@ -1,5 +1,7 @@
 package org.cognitor.cassandra;
 
+import static org.cassandraunit.utils.EmbeddedCassandraServerHelper.DEFAULT_CASSANDRA_YML_FILE;
+
 import org.cassandraunit.CQLDataLoader;
 import org.cassandraunit.dataset.CQLDataSet;
 import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
@@ -8,8 +10,6 @@ import org.junit.rules.ExternalResource;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
-
-import static org.cassandraunit.utils.EmbeddedCassandraServerHelper.DEFAULT_CASSANDRA_YML_FILE;
 
 /**
  * Cassandra Rule class that loads data from a specified point to initialize the database.
@@ -26,7 +26,7 @@ public class CassandraJUnitRule extends ExternalResource {
     private static final long TIMEOUT = 60000L;
     private static final String LOCALHOST = "127.0.0.1";
 
-    private final Cluster cluster;
+    private Cluster cluster;
     private final CQLDataSet dataSet;
     private final String ymlFileLocation;
 
@@ -35,8 +35,7 @@ public class CassandraJUnitRule extends ExternalResource {
     }
 
     public CassandraJUnitRule(String dataSetLocation, String ymlFileLocation) {
-        dataSet = new ClassPathCQLDataSet(dataSetLocation, TEST_KEYSPACE);
-        cluster = new Cluster.Builder().addContactPoints(LOCALHOST).withPort(9142).build();
+        this.dataSet = new ClassPathCQLDataSet(dataSetLocation, TEST_KEYSPACE);
         this.ymlFileLocation = ymlFileLocation;
     }
 
@@ -50,11 +49,15 @@ public class CassandraJUnitRule extends ExternalResource {
     @Override
     protected void before() throws Exception {
         EmbeddedCassandraServerHelper.startEmbeddedCassandra(ymlFileLocation, TIMEOUT);
+        // this is required so that the EmbeddedCassandraServerHelper does not fail with NPE
+        this.cluster = EmbeddedCassandraServerHelper.getCluster();
         load();
     }
 
     @Override
     protected void after() {
+        // this is required so that the EmbeddedCassandraServerHelper does not fail with NPE
+        EmbeddedCassandraServerHelper.getSession();
         EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
     }
 
