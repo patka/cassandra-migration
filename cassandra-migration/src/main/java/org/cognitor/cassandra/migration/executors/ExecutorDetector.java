ea95657a86279c83d44163ad70354a8c651a30f5
Mihai Vasilache <mihai.vasilache@edenred.com>
2020-12-29 08:05:08 +0200

    detect amazon keyspaces even schema metadata is disabled

--- cassandra-migration/src/main/java/org/cognitor/cassandra/migration/executors/ExecutorDetector.java
+++ cassandra-migration/src/main/java/org/cognitor/cassandra/migration/executors/ExecutorDetector.java
@@ -1,8 +1,14 @@
 package org.cognitor.cassandra.migration.executors;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExecutorDetector {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     private final CqlSession session;
 
     private final String mcsSchemaName = "system_schema_mcs";
@@ -14,18 +20,28 @@ public class ExecutorDetector {
 
     public Executor getExecutor() {
         if(isAwsMcs()) {
+            LOGGER.debug("Amazon Keyspaces cluster detected.");
             return new AwsMcsExecutor(session);
         }
         return new SimpleExecutor(session);
     }
 
     private boolean isAwsMcs() {
+        //metadata schemas can be disabled with datastax-java-driver.advanced.metadata.schema.enabled = "false"
+        return hasMcsSchema() || hasAmazonKeyspaceInClusterName();
+    }
+
+    private boolean hasMcsSchema() {
         if(!session.getMetadata().getKeyspace(mcsSchemaName).isPresent()){
             return false;
         }
+        return true;
+    }
+
+    private boolean hasAmazonKeyspaceInClusterName() {
         if(!session.getMetadata().getClusterName().isPresent()) {
             return false;
         }
-        return session.getMetadata().getClusterName().get().equalsIgnoreCase(mcsClusterName);
+        return session.getMetadata().getClusterName().get().toLowerCase().contains(mcsClusterName.toLowerCase());
     }
 }
